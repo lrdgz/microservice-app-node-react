3ab62d1a5f6b574ac185a32878973775ff596dc2
lrdgz <developer0606@gmail.com>
2022-08-18 12:02:13 -0400

    Comments index example added

--- comments/index.js
+++ comments/index.js
@@ -7,24 +7,26 @@ const { randomBytes } = require("crypto");
 const app = express();
 app.use(bodyParser.json());
 
-const posts = {};
+const commentsByPostsId = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostsId[req.params.id] || []);
 });
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
 
-    const {title} = req.body;
+    const { content } = req.body;
 
-    posts[id] = {
-        id, title
-    };
+    const comments = commentsByPostsId[req.params.id] || [];
 
-    res.status(201).send(posts[id]);
+    comments.push({id: commentId, content})
+
+    commentsByPostsId[req.params.id] = comments;
+
+    res.status(201).send(comments);
 });
 
-app.listen(4000, () => {
-    console.log('Listening on port 4000');
+app.listen(4001, () => {
+    console.log('Listening on port 4001');
 })
