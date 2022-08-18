87408a5fd5d4c5cd4e02c290b90cce4bc5b07878
lrdgz <developer0606@gmail.com>
2022-08-18 14:59:42 -0400

    Added final configuration

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,8 +7,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
     
     axios.post("http://localhost:4000/events", event);
     axios.post("http://localhost:4001/events", event);
@@ -16,6 +20,10 @@ app.post('/events', (req, res) => {
     axios.post("http://localhost:4003/events", event);
 
     res.send({ status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 
