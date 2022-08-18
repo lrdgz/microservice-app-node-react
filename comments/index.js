8c2ad62db78cfa64a715bfaf649c4a0e6ffc6d2e
lrdgz <developer0606@gmail.com>
2022-08-18 13:41:20 -0400

    Add query and event bus setting

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const { randomBytes } = require("crypto");
 
@@ -15,7 +16,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostsId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
 
     const { content } = req.body;
@@ -26,7 +27,21 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostsId[req.params.id] = comments;
 
+    await axios.post("http://localhost:4005/events", {
+        type: 'CommentCreated', 
+        data: {
+            id: commentId, 
+            content,
+            postId: req.params.id
+        }, 
+    }).catch(err => console.log(err));
+
     res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
 });
 
 app.listen(4001, () => {
