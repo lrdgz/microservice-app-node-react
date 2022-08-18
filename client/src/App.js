9851320451aaff29440ba6da4938c013fae79466
lrdgz <developer0606@gmail.com>
2022-08-18 12:43:38 -0400

    Add react connection with the microservices

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PostList from "./PostList";
 import PostCreate from "./PostCreate";
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
       <div className="container">
         <h1>Create Post</h1>
         <PostCreate />
+
+        <hr />
+
+        <h1>Posts</h1>
+        <PostList />
       </div>
     </>
   );
