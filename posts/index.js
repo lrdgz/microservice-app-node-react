7a8c000a6de4fb33161fb459949ffc695ae135e3
lrdgz <developer0606@gmail.com>
2022-08-19 09:06:46 -0400

    Change image URI to service url name

--- posts/index.js
+++ posts/index.js
@@ -25,8 +25,8 @@ app.post('/posts', async (req, res) => {
         id, title
     };
 
-    await axios.post("http://localhost:4005/events", {
-       type: 'PostCreated', 
+    await axios.post("http://event-bus-srv:4005/events", {
+       type: 'PostCreated',
        data: {
         id, 
         title
